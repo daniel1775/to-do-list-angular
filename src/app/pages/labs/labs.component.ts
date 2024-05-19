b2cacd406f505d4e678bab542f55a7585bf8d78c
daniel1775 <developercressco@Developers-MacBook-Air.local>
2024-05-19 13:46:38 -0500

    feat: events

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -2,13 +2,32 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-labs',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './labs.component.html',
-  styleUrl: './labs.component.sass',
+    selector: 'app-labs',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './labs.component.html',
+    styleUrl: './labs.component.css',
 })
 export class LabsComponent {
-  title = 'to-do-list-angular';
-  tasks = ['Task 1', 'Task 2', 'Task 3'];
+    title = 'to-do-list-angular';
+    tasks = ['Task 1', 'Task 2', 'Task 3'];
+    isDisabled = false
+
+    handleClick() {
+        alert('Button clicked!')
+    }
+    handleDoubleClick() {
+        alert('Button double clicked!')
+    }
+    handleInput(event: Event) {
+        console.log((event.target as HTMLInputElement).value);
+    }
+    handleKeyDown(event: KeyboardEvent) {
+        const input = event.target as HTMLInputElement
+        alert(`Keydown event!\n${input.value}`)
+    }
+    handleKeyDownMoreKey(event: Event) {
+        const input = event.target as HTMLInputElement
+        alert(`Keydown event!\n${input.value}`)
+    }
 }
